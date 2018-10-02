3e610821ed3d8cc4ca4751975d996ba0eae0b29e
B533P-10\533 <talbot.nicolas.nt@gmail.com>
2018-10-02 16:39:20 -0400

    question 1 et 2

---
+++ Assets/Scripts/mouvement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouvement : MonoBehaviour {
+    private Rigidbody planeRigidBody;
+    [SerializeField] float forward = 3;
+    private ParticleSystem smoke;
+    // Use this for initialization
+    void Start() {
+        planeRigidBody = GetComponent<Rigidbody>();
+        smoke = GetComponentInChildren<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetAxis("Jump")>0)
+        {
+            planeRigidBody.AddRelativeForce(new Vector3(0, 0, forward)*Time.deltaTime*500);
+        }
+        if(planeRigidBody.velocity.z >0)
+        {
+            smoke.Play();
+        }
+        else
+        {
+            smoke.Stop();
+        }
+	}
+
+    public Vector3 getSpeed()
+    {
+        return planeRigidBody.velocity;
+    }
+}
---
+++ Assets/Scripts/rotationElice.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationElice : MonoBehaviour {
+    [SerializeField] public float vitesseRotation;
+    private mouvement avion;
+    
+    // Use this for initialization
+    void Start () {
+        avion = GetComponentInParent<mouvement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       transform.Rotate(avion.getSpeed());
+    }
+}
